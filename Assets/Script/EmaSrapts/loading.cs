342b856aeef63276814b52272e5b9b585f6c58c6
ema crapa <emanuele.crapanzano02@gmail.com>
2024-05-27 09:54:06 +0200

    preload scene

--- Assets/Script/EmaSrapts/loading.cs
+++ Assets/Script/EmaSrapts/loading.cs
@@ -8,8 +8,15 @@ public class LoaderCallback : MonoBehaviour
 {
     private bool _isFirstUpdate = true;
     private float _timer;
+    private AsyncOperation _asyncLoad;
 
     [SerializeField] private float _loadingScreenDuration = 3.0f;
+
+    private void Start()
+    {
+        _asyncLoad = SceneManager.LoadSceneAsync(2);
+        _asyncLoad.allowSceneActivation = false;
+    }
 
     private void Update()
     {
@@ -24,7 +31,7 @@ public class LoaderCallback : MonoBehaviour
             _timer -= Time.deltaTime;
             if (_timer <= 0)
             {
-                SceneManager.LoadScene(2);
+                _asyncLoad.allowSceneActivation = true;
             }
         }
     }
